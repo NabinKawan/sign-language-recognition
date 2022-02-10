67519060b7be73d82b1793a7ed19c9479b21a2f0
Nabin Kawan <nabin.kawan.9@gmail.com>
2022-02-10 13:00:23 +0545

    Data resolve

---
+++ test.py
@@ -0,0 +1,22 @@
+# from msilib import sequence
+# import numpy as np
+
+# sequence=[]
+# arr=np.zeros([10,1662],dtype=float)
+# lst=[]
+# for e in arr:
+#     # print(e)
+#     lst.append(list(e))
+# # arr=list(arr)
+# print(len(lst))
+# # print(list(arr))
+# # print(arr.shape)
+# sequence.append(lst)
+# # print(sequence)
+
+import time
+start_time=time.time()
+time.sleep(2)
+end_time=time.time()
+elapsed_time=end_time-start_time
+print(int(elapsed_time))
